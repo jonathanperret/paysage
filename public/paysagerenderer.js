f1d6a3037af5d97532628b2c8a61d834400a8638
Julien <juliendorra@juliendorra.com>
2014-05-31 11:36:50 +0200

    removed "force transparent background" JS
    1. doesn't work
    2. let people (and IDEs) create the rules
    3. seems to break rendering of static scripts (like :
    http://processing.org/examples/shapeprimitives.html)

--- public/paysagerenderer.js
+++ public/paysagerenderer.js
@@ -19,7 +19,7 @@ io.on('code update', function(data) {
     console.log (data);
     var id = data.codeid;
     var code = data.code;
-    console.log (id);
+    console.log ('code received '+id);
 
     canvas[id] = document.createElement('canvas');
     canvas[id].setAttribute('id', id);
@@ -27,6 +27,7 @@ io.on('code update', function(data) {
     if (!document.getElementById(id)) {
       
       document.getElementById('container').appendChild(canvas[id]);
+      console.log ('canvas created');
     }
     else {
      var oldcanvas = document.getElementById(id);
@@ -35,13 +36,6 @@ io.on('code update', function(data) {
     }
 
     sketch[id] = new Processing(canvas[id], code);
-
-    // trying to force a transparent background
-    //The Internetz say: "To enable transparent backgrounds for a js-built sketch, you must set the sketch transparency property to true.""
-    sketch[id].externals.sketch.options.isTransparent = true; // But not sure it's necessary... 
-    // because this set the background transparent for the sketch as well:
-    sketch[id].background(0,0); // works just for a frame, then is changed back by the loop to the original value :-(
-
 
 });
 
