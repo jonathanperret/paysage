9695a96ecfc2430d3b4f609f52dda1651b26696b
Jonathan Perret <jonathan@ut7.fr>
2015-09-30 13:05:13 +0200

    Split out programmer.js

---
+++ public/programmer.js
@@ -0,0 +1,67 @@
+(function() {
+  "use strict";
+
+  io = io.connect();
+
+  document.getElementById('bouton').addEventListener('click',
+    function(){
+      var codeid = document.getElementById('codeid').value;
+      var playgroundid = document.getElementById('playgroundid').value;
+      var code = document.getElementById('code').value;
+      var data = {codeid: codeid, playgroundid: playgroundid, code: code}
+      io.emit('code update', data);
+    });
+
+  io.on('objects full update', function (data) {
+    var playgroundId = data.playgroundId,
+        objectIds = data.objectIds,
+        $objects = $("#objects");
+    $objects.empty();
+    objectIds.forEach(function (objectId) {
+      var $item = $("<li><a href='#'>" + objectId + "</a></li>");
+      $item.click(function () {
+        var data = {playgroundId: playgroundId, objectId: objectId};
+        io.emit('request code', data);
+      });
+      $objects.append($item);
+    });
+  });
+
+  io.on('source code', function (data) {
+    $("#playgroundid").val(data.playgroundId);
+    $("#codeid").val(data.objectId);
+    $("#code").val(data.code);
+  });
+
+  io.emit('programmer up', document.getElementById('playgroundid').value);
+
+  $('.example').click(function () {
+    $('#codeid').val(chance.word());
+    $('#code').val($('script', this).html());
+  });
+
+  $(function() { $('#codeid').val(chance.word()); });
+
+  var frozenpreview = null;
+
+  var switchframe = function () {
+    if (frozenpreview) {
+      $('.viewer-part').show(150);
+      frozenpreview.appendTo('#viewercontainer');
+      frozenpreview = null;
+    } else {
+      frozenpreview = $( '#viewerframe' ).detach();
+      $('.viewer-part').hide(150);
+    }
+  }
+
+  var frameoff = function () {
+    if (frozenpreview === null) {
+      frozenpreview = $('#viewerframe').detach();
+      $('.viewer-part').hide(150);
+    }
+  }
+
+  $('#previewonoff').click(switchframe);
+  $('#openinnewwindow').click(frameoff);
+}());
