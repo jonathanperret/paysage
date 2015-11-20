3baa6ad2efc26a77fd5e17aab68126bf0418f121
Julien <juliendorra@juliendorra.com>
2015-11-20 12:46:13 +0100

    preparing for multiple examples

--- public/workshopfiles/creature/creaturenamefromhash.js
+++ public/workshopfiles/creature/creaturenamefromhash.js
@@ -2,7 +2,7 @@
   var creaturename = window.location.hash.slice(1);
   console.log(creaturename);
 
-   $('.creature').click(function () {
+   $('.example').click(function () {
     $('#codeid').val(creaturename);
     $('#code').val($('script', this).html());
   });
