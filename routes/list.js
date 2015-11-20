68f26a29b8a3c17a9c6ef2488f9ac701fe88ca2d
Julien <juliendorra@juliendorra.com>
2015-11-20 01:01:00 +0100

    the list page is now called list everywhere
    The /list page was referenced as home (in app.js) or index (in routes
    and views). Confusing.

--- routes/list.js
+++ routes/list.js
@@ -3,8 +3,8 @@ module.exports = function(codeObjects) {
   var router = express.Router();
 
   router.get('/', function(req, res) {
-    res.render('index', { title: 'Paysage', playgrounds: codeObjects });
+    res.render('list', { title: 'Paysage', playgrounds: codeObjects });
   });
 
   return router;
-}
+};
