6d597bf946a76b2e43ea8bfed4e88d2f0aa4aa71
Julien <juliendorra@juliendorra.com>
2015-11-25 04:25:58 +0100

    setCodeId has a better home in the code initialization script
    The code initialization script is tasked with setting ids, loading code
    from example, etc.
    The source builder return the source code to send to the server. This
    one is very simple, but separating it allows for more complex one
    (creaturesourcebuilder for example) to be super easy to add.

--- public/programmerjs/codeinitialization.js
+++ public/programmerjs/codeinitialization.js
@@ -1,4 +1,13 @@
-// on load generating a random name for new empty code
+function setCodeId(codeId) {
+  $('#codeid').val(codeId);
+  window.location.hash=codeId;
+}
+
+function createCodeId() {
+  setCodeId(chance.word());
+}
+
+// on load generating a random name if no name is passed via the URL Fragmemt identifier
 
 $(function() {
   if (window.location.hash) {
@@ -8,18 +17,15 @@ $(function() {
   }
 });
 
+
 // loading code from an example and generating a random name
-//
-function createCodeId() {
-  setCodeId(chance.word());
-}
 
 $('.example').click(function() {
   createCodeId();
   $('#code').val($('script', this).html());
 });
 
-// drag and dropping a text file and naming the code fron the file name
+// drag and dropping a text file and naming the code from the file name
 // thanks http://stackoverflow.com/questions/12214057/drag-n-drop-text-file
 // (after trying to use http://filedropjs.org/ )
 
--- public/programmerjs/sourcebuilder.js
+++ public/programmerjs/sourcebuilder.js
@@ -1,3 +1,11 @@
+// A separate sourcebuilder.js script allow different Programmer pages 
+// (for example a dedicated programme page for a workshop like Code Creatures) 
+// to reuse the receptiontransmission.js script
+// while building the code to emit to the server in their own way.
+// (for example the Creature Programmer insert the user code into a Processing script complete with a class definition)
+
+// the sourcebuilder only concern is to build and give a code ready to be sent to the paysage server
+
 getCompleteSource = function(callback) {
 
   sourcecode = document.getElementById('code').value;
@@ -6,8 +14,4 @@ getCompleteSource = function(callback) {
 
 };
 
-function setCodeId(codeId) {
-  $('#codeid').val(codeId);
-  window.location.hash=codeId;
-}
 
