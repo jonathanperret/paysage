723dc47ffd2de9b4e42e50c966875a7965c8ff1f
Julien <juliendorra@juliendorra.com>
2015-11-25 04:19:33 +0100

    creaturefromhash.js is now creaturecodeinitialization.js

---
+++ public/workshopfiles/creature/creaturecodeinitialization.js
@@ -0,0 +1,23 @@
+// thanks https://gist.github.com/jlong/2428561
+var creaturename = window.location.hash.slice(1);
+
+function setCodeId(codeId) {
+  window.location.hash=codeId;
+  $('#codeid').html(creaturename);
+}
+
+if (!creaturename) {
+  creaturename = chance.word();
+  window.location.hash = creaturename;
+}
+
+// loading code from an example
+
+$('.example').click(function () {
+  $('#code').val( $('script', this).html() );
+});
+
+
+$(function() {
+  $('#codeid').html(creaturename);
+});
