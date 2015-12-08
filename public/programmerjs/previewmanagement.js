30de2d3e6476d7328dc008ea61722a5be0e655dc
Julien <juliendorra@juliendorra.com>
2015-12-08 08:30:39 +0100

    hide only the frame, not controls

--- public/programmerjs/previewmanagement.js
+++ public/programmerjs/previewmanagement.js
@@ -3,19 +3,19 @@ var frozenpreview = null;
 var switchframe = function(event) {
   event.preventDefault();
   if (frozenpreview) {
-    $('.viewer-part').show(150);
+    $('#viewercontainer').show(150);
     frozenpreview.appendTo('#viewercontainer');
     frozenpreview = null;
   } else {
     frozenpreview = $('#viewerframe').detach();
-    $('.viewer-part').hide(150);
+    $('#viewercontainer').hide(150);
   }
 };
 
 var frameoff = function() {
   if (frozenpreview === null) {
     frozenpreview = $('#viewerframe').detach();
-    $('.viewer-part').hide(150);
+    $('#viewercontainer').hide(150);
   }
 };
 
