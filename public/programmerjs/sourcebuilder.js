5a20e0c2a34c61d0e958c110625ba338ecfbfc7a
Julien <juliendorra@juliendorra.com>
2015-12-10 15:18:51 +0100

    Source builders add Client and Mediatype info to the CodeObject
    By adding a Client and Mediatype info the Programmer can ask for its
    own code only, or code it knows how to manage. The Playground renderer
    can also make informed decision on how to treat the code received (ex.
    not creating a canvas for d3 code)

--- public/programmerjs/sourcebuilder.js
+++ public/programmerjs/sourcebuilder.js
@@ -1,17 +1,28 @@
 // A separate sourcebuilder.js script allow different Programmer pages 
-// (for example a dedicated programme page for a workshop like Code Creatures) 
+// (for example the dedicated programmer page for a workshop like Code Creatures) 
 // to reuse the receptiontransmission.js script
 // while building the code to emit to the server in their own way.
 // (for example the Creature Programmer insert the user code into a Processing script complete with a class definition)
 
-// the sourcebuilder only concern is to build and give a code ready to be sent to the paysage server
+// the sourcebuilder only concern is to build and give a code object ready to be sent to the paysage server
 
-getCompleteSource = function(callback) {
+getCompleteCodeObject = function (callback) {
 
-  sourcecode = document.getElementById('code').value;
-  console.log(sourcecode);
-  callback(sourcecode);
-
+    var codeid = document.getElementById('codeid').value || document.getElementById('codeid').textContent;
+    var playgroundid = document.getElementById('playgroundid').value;
+    var mediatype = "text/processing";
+    var client = "programmer";
+    var code = document.getElementById('code').value;
+    
+    var data = {
+        codeid: codeid,
+        playgroundid: playgroundid,
+        mediatype: mediatype,
+        client: client,
+        code: code
+    };
+    
+  callback(data);
 };
 
 
--- public/workshopfiles/creature/creaturesourcebuilder.js
+++ public/workshopfiles/creature/creaturesourcebuilder.js
@@ -1,23 +1,41 @@
-getCompleteSource = function(callback) {
+// A separate sourcebuilder.js script allow different Programmer pages 
+// (for example the dedicated programmer page for a workshop like Code Creatures) 
+// to reuse the receptiontransmission.js script
+// while building the code to emit to the server in their own way.
+// (for example the Creature Programmer insert the user code into a Processing script complete with a class definition)
 
-var sourcecode = "";
+// the sourcebuilder only concern is to build and give a code object ready to be sent to the paysage server
 
- $.get(
+getCompleteCodeObject = function (callback) {
 
-    "/workshopfiles/creature/creature.pde",
+    var codeid = document.getElementById('codeid').value || document.getElementById('codeid').textContent;
+    var playgroundid = document.getElementById('playgroundid').value;
+    var mediatype = "text/processing";
+    var client = "creature";
+    var code = document.getElementById('code').value;
 
-    function(originalsource) {
+    $.get(
 
-      var template = originalsource;
-      var usercode = document.getElementById('code').value;
+        "/workshopfiles/creature/creature.pde",
 
-      var match = /\/\/kids code here/;
+        function (originalsource) {
 
-      sourcecode = template.replace(match, usercode);
-      callback(sourcecode);
+            var template = originalsource;
+            var match = /\/\/kids code here/;
 
-    },
+            completecode = template.replace(match, code);
 
-    "text");
+            var data = {
+                codeid: codeid,
+                playgroundid: playgroundid,
+                mediatype: mediatype,
+                client: client,
+                code: completecode
+            };
+            callback(data);
+
+        },
+
+        "text");
 
 };
