287e0c5f7328349b3ac5f2fbd0775fc3e68e7397
Jonathan Perret <jonathan@ut7.fr>
2016-05-22 18:03:12 +0200

    Remove static-favicon middleware
    Besides being obsolete, it prevented our actual favicon from being served

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var codeObjects = {};
 
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -15,7 +14,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('hbs', exphbs());
 app.set('view engine', 'hbs');
 
-app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
