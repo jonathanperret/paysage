b40886d32ee57e4e46cc13f18edd9079bc92b4aa
Étienne Charignon <etienne.charignon@gmail.com>
2017-06-13 12:24:39 +0200

    List view : case when there is no playground

--- routes/list.js
+++ routes/list.js
@@ -3,7 +3,11 @@ module.exports = function(codeObjects) {
   var router = express.Router();
 
   router.get('/', function(req, res) {
-    res.render('list', { title: 'Paysage', playgrounds: codeObjects });
+    res.render('list', {
+      title: 'Paysage',
+      playgrounds: codeObjects,
+      hasPlayground: Object.keys(codeObjects).length > 0
+    });
   });
 
   return router;
