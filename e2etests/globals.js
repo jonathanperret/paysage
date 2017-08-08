8bcfcdba0c94b7d853977753e814dfb15dfb9760
Jonathan Perret <jonathan@ut7.fr>
2017-08-08 17:04:26 +0200

    Use a page object

---
+++ e2etests/globals.js
@@ -0,0 +1,33 @@
+const chromedriver = require('chromedriver');
+
+process.env.TESTING = 'testing';
+const server = require('../app');
+
+module.exports = {
+  before: function(done) {
+    console.log("starting server");
+    server.listen(0, ()=>{
+      this.test_settings.launch_url = 'http://127.0.0.1:' + server.address().port;
+      console.log("starting chromedriver");
+      const driverChild = chromedriver.start();
+      driverChild.on('error',
+                     (err)=>console.log("chromedriver start error", err));
+      driverChild.on('exit',
+                     (exitCode, signal)=>
+                      console.log("chromedriver exited with",
+                                  signal ? ("signal " + signal) : ("code " + exitCode)));
+      console.log("chromedriver started, pid", driverChild.pid);
+
+      setTimeout(()=>{
+        done(driverChild.pid ? null : new Error("could not start chromedriver"));
+      }, 2000);
+    });
+  },
+
+  after: function(done) {
+    console.log("stopping chromedriver");
+    chromedriver.stop();
+    console.log("stopping server");
+    server.close(done);
+  },
+};
--- e2etests/page_objects/programmer.js
+++ e2etests/page_objects/programmer.js
@@ -2,5 +2,17 @@ module.exports = {
   url: function() {
     return this.api.launchUrl;
   },
-  elements: {}
+  elements: {
+    'go-live': { selector: '#bouton' }
+  },
+  commands: [{
+    setCode: function(code) {
+      this.api.execute(`
+        var ta = document.querySelector("#code textarea");
+        ta.value = "${code.replace(/\\/,'\\\\').replace(/"/,'"')}";
+        ta.dispatchEvent(new Event("input"));
+      `);
+      return this;
+    }
+  }]
 };
