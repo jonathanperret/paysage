11afd72c08494e2fb7d46063804ab439eed83ab3
Raphael Pierquin <raphael@ut7.fr>
2017-08-10 08:08:07 +0200

    Remove uglyness

--- routes/list.js
+++ routes/list.js
@@ -3,11 +3,9 @@ module.exports = function(world) {
   var router = express.Router();
 
   router.get('/', function(req, res) {
-    var playgrounds = {};
-    world.tour().forEach(function(name){playgrounds[name]={};})
     res.render('list', {
       title: 'Paysage',
-      playgrounds: playgrounds,
+      playgrounds: world.tour(),
       hasPlayground: world.tour().length > 0
     });
   });
