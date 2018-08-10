5397f004f11a4c6b0279ecd312b03b1c78f4d0a6
Étienne Charignon <etienne.charignon@gmail.com>
2018-08-10 22:29:08 +0200

    Use the url hash (fragment identifier) on the renderer side to manage code objects' visibility

---
+++ spec/public/paysagerenderer/visibility_management_spec.js
@@ -0,0 +1,43 @@
+/* global describe, beforeEach, it, expect */
+/* global Paysage */
+describe('The Paysage renderer visibility management', function () {
+  it('can parse Url hash for code objects ids to display', function () {
+    expect(Paysage.readIdsFromUrlHash('')).toEqual(undefined);
+    expect(Paysage.readIdsFromUrlHash('#')).toEqual(undefined);
+    expect(Paysage.readIdsFromUrlHash('#only=toto')).toEqual(['toto']);
+    expect(Paysage.readIdsFromUrlHash('#only=toto,titi')).toEqual(['toto', 'titi']);
+    expect(Paysage.readIdsFromUrlHash('#only=')).toEqual([]);
+  });
+
+  describe('has code object filtering', function () {
+    var shownIds;
+    var hiddenIds;
+
+    var show = function (id) {
+      shownIds.push(id);
+    };
+
+    var hide = function (id) {
+      hiddenIds.push(id);
+    };
+
+    beforeEach(function () {
+      shownIds = [];
+      hiddenIds = [];
+    });
+
+    it('that show all code objects when no id is given', function () {
+      Paysage.showCodeObjects(['toto', 'titi'], undefined, show, hide);
+
+      expect(shownIds).toEqual(['toto', 'titi']);
+      expect(hiddenIds).toEqual([]);
+    });
+
+    it('that show only requeted code objects', function () {
+      Paysage.showCodeObjects(['toto', 'titi'], ['toto'], show, hide);
+
+      expect(shownIds).toEqual(['toto']);
+      expect(hiddenIds).toEqual(['titi']);
+    });
+  });
+});
